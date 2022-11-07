20c993d98a7e0f23bf334a2febbf09c585291bfc
Fahad Bhatti OSO <83200973+fahadbhatti2001@users.noreply.github.com>
2022-11-07 12:34:28 +0500

    HashRouter and SVG Compress

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Header } from "@/Components";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { Dashboard, Login } from "@/Components";
 import { Footer } from "./Components/Footer";
 
 export const App = () => {
   return(
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header/>
           <Routes>
             <Route path="/" element={<Dashboard/>}/>
             <Route path="/login" element={<Login/>}/>
           </Routes>
         <Footer/>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 };
